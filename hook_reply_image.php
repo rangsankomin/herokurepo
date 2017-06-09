430d496b79a9dd1db119a782231f5c889afcff7e
DESKTOP-B6OQNIJ\rangs <rangsankomin@yahoo.com>
2017-06-09 21:09:01 +0700

    no message

--- hook_reply_image.php
+++ hook_reply_image.php
@@ -11,7 +11,7 @@ if (empty($configs) || empty($configs['channel_token'])) {
   return;
 }*/
 
-$channel_token = '6818df0cecde4786f6fc177155cc940d:X3LZvU8+/mXKb/sJuuW7IumF/7kV3BYvPG2x+gmeMHGDrNxwzawcUUWS/7Cnm7l94neWZM1M/tAC33QrvZK4HRLz7I3c/gBHkXYui3w2WvhqZWT/qNMl0Qfnxjv4+qtk4S+waflWXYISoJEq3tTEOQdB04t89/1O/w1cDnyilFU=';
+$channel_token = 'X3LZvU8+/mXKb/sJuuW7IumF/7kV3BYvPG2x+gmeMHGDrNxwzawcUUWS/7Cnm7l94neWZM1M/tAC33QrvZK4HRLz7I3c/gBHkXYui3w2WvhqZWT/qNMl0Qfnxjv4+qtk4S+waflWXYISoJEq3tTEOQdB04t89/1O/w1cDnyilFU=';
 
 
 $body = file_get_contents('php://input');
