f84637dba5e9c7540ae670900a200edeac6a4d80
DESKTOP-B6OQNIJ\rangs <rangsankomin@yahoo.com>
2017-06-09 20:56:46 +0700

    no message

--- hook_reply_image.php
+++ hook_reply_image.php
@@ -2,16 +2,16 @@
 
 require_once './vendor/autoload.php';
 
-use Symfony\Component\Yaml\Yaml;
+//use Symfony\Component\Yaml\Yaml;
 
 setlocale(LC_CTYPE, "en_US.UTF-8");
 
-$configs = Yaml::parse(file_get_contents('./config.yml'));
+/*$configs = Yaml::parse(file_get_contents('./config.yml'));
 if (empty($configs) || empty($configs['channel_token'])) {
   return;
-}
+}*/
 
-$channel_token = 'X3LZvU8+/mXKb/sJuuW7IumF/7kV3BYvPG2x+gmeMHGDrNxwzawcUUWS/7Cnm7l94neWZM1M/tAC33QrvZK4HRLz7I3c/gBHkXYui3w2WvhqZWT/qNMl0Qfnxjv4+qtk4S+waflWXYISoJEq3tTEOQdB04t89/1O/w1cDnyilFU=';
+$channel_token = '6818df0cecde4786f6fc177155cc940d:X3LZvU8+/mXKb/sJuuW7IumF/7kV3BYvPG2x+gmeMHGDrNxwzawcUUWS/7Cnm7l94neWZM1M/tAC33QrvZK4HRLz7I3c/gBHkXYui3w2WvhqZWT/qNMl0Qfnxjv4+qtk4S+waflWXYISoJEq3tTEOQdB04t89/1O/w1cDnyilFU=';
 
 
 $body = file_get_contents('php://input');
